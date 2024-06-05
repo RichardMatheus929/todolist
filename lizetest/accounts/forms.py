8c59b16a4f2cb5e103ec55b0d49cca5d9aca3a5b
Richard Matheus <richardmatheus929@gmail.com>
2024-06-05 19:39:27 -0300

    add verification of confirmation password

--- lizetest/accounts/forms.py
+++ lizetest/accounts/forms.py
@@ -1,9 +1,27 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.conf import settings 
+from django.conf import settings
 from .models import User
+from django.contrib.auth import password_validation
+
 
 class CustomUserCreationForm(UserCreationForm):
+
+    password1 = forms.CharField(
+        label=("Senha"),
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={"autocomplete": "new-password", "id": "password1"}),
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+
+    password2 = forms.CharField(
+        label=("Confirmação de senha"),
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password","id":"password2"}),
+        strip=False,
+        help_text=('<strong id="aviso_text"></strong>'),
+    )
+
     class Meta:
         model = User
-        fields = ['name','username', 'email', 'password1', 'password2']
+        fields = ['name', 'username', 'email', 'password1', 'password2']
