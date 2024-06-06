43a278e891c261a4a7bde4bc1d425123a6e7b9af
Richard Matheus <richardmatheus929@gmail.com>
2024-06-06 13:39:02 -0300

    add filter of user for categorys

--- lizetest/category/models.py
+++ lizetest/category/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from lizetest.core.models import BaseModel
 
+from lizetest.accounts.models import User
+
 class Category(BaseModel):
     name = models.CharField(max_length=255)
+    author = models.ForeignKey(User,on_delete=models.CASCADE,blank=True)
 
     def __str__(self):
         return self.name
--- lizetest/category/views.py
+++ lizetest/category/views.py
@@ -2,6 +2,7 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from lizetest.core.models import BaseViews
+from lizetest.todo.models import Task
 from .models import Category
 from .forms import CategoryForm
 
@@ -11,11 +12,18 @@ class CategoryListView(ListView,BaseViews):
     template_name = 'category_list.html'
     context_object_name = 'categorias'
 
+    def get_queryset(self):
+        return Category.objects.filter(author=self.request.user)
+
 class CategoryCreateView(CreateView,BaseViews):
     model = Category
     form_class = CategoryForm
     template_name = 'category_form.html'
     success_url = reverse_lazy('category:category_list')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 class CategoryUpdateView(UpdateView,BaseViews):
     model = Category
